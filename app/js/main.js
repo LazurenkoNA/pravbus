31b5609609266624fb3e41ff8a59fbb88b8a1de1
Nikita Lazurenko <naj040315@gmail.com>
2020-04-29 19:02:56 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,47 @@
+const selectedAll = document.querySelectorAll(".select-box__current");
+
+selectedAll.forEach((selected) => {
+  const selectedBody = selected.previousElementSibling;
+  const selectedList = selectedBody.querySelectorAll(".select-box__body-item");
+
+  selected.addEventListener("click", () => {
+    if (selectedBody.classList.contains("active")) {
+      selectedBody.classList.remove("active");
+    } else {
+      let currentActive = document.querySelector(".select-box__body.active");
+
+      if (currentActive) {
+        currentActive.classList.remove("active");
+      }
+      selectedBody.classList.add("active");
+    }
+  });
+
+  selectedList.forEach((o) => {
+    o.addEventListener("click", () => {
+      selected.innerHTML = o.querySelector("label").innerHTML;
+      selectedBody.classList.remove("active");
+    });
+  });
+});
+
+// $(document).ready(function () {
+//   $(".slider").slick({
+//     dots: true,
+//     arrows: false,
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 2,
+//   });
+// });
+
+const gallerySwiper = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 5,
+  freeMode: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+    // clickable: true,
+  },
+});
